30c6904741f394f52dcc1bde4e47ad556c9cbdfa
WonderinSoul118 <67811767+YeetAtTheBluWolf@users.noreply.github.com>
2022-07-21 18:22:08 -0400

    Adding structure to the program Still needs a lot of work...

---
+++ Wireguard-FrontEnd/Backend/PowerShell/PowerShellWGShow.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Wireguard_FrontEnd.Backend.PowerShell
+{
+    internal class PowerShellWGShow
+    {
+    }
+}
