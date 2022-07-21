400136b6f1d283f95b9bbfdfdcb7a96ef2e37d26
WonderinSoul118 <67811767+YeetAtTheBluWolf@users.noreply.github.com>
2022-07-21 12:48:01 -0400

    Created a ComboBox to detect all the wireguard conf files and chose the primarary file to launch the tunnel with.

---
+++ Wireguard-FrontEnd/Backend/VpnSelectionDetection.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Wireguard_FrontEnd.Backend
+{
+    public class VpnSelectionDetection
+    {
+
+        private static string _path = @"C:\Program Files\WireGuard\Data\Configurations";
+        private static readonly DirectoryInfo FileDetections = new(_path);
+
+        public VpnSelectionDetection(string path = @"C:\Program Files\WireGuard\Data\Configurations")
+        {
+            _path = path;
+        }
+
+        public List<string> FilesListed()
+        {
+            List<string> filesDetected = new List<string>();
+            try
+            {
+                if (FileDetections != null)
+                    foreach (var i in FileDetections.GetFiles())
+                    {
+                        filesDetected.Add(i.FullName);
+                    }
+
+            }
+            catch (NullReferenceException e)
+            {
+                Console.WriteLine(e + ": Detected no files");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e + ": Detected Error maybe run program in administrator");
+            }
+
+            return filesDetected;
+        }
+    }
+}
